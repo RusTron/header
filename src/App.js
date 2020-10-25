4fcb04289ceac462ee5202ee4a1f2cb84b47077a
RusTron <rusan.tron@gmail.com>
2020-10-26 01:36:29 +0200

    pilot

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React, { useState } from 'react';
 import logo from './logo.png';
-import menu from './Vector.png';
-import closeMenu from './x.png';
+import { FacebookOutlined, TwitterOutlined, LinkedinOutlined } from '@ant-design/icons';
 import classNames from 'classnames';
-import './App.scss';
+import Select from 'react-select';
 import { NavLink } from 'react-router-dom';
+import './App.scss';
 
-const BUTTONS = {
+const SELECT = {
   one: 'One',
   two: 'Two',
   three: 'Three',
 }
 
+const MENU = {
+  closed: 'closed',
+  visible: 'visible',
+}
+
+const options = [
+  { value: 'chocolate', label: 'item 1' },
+  { value: 'strawberry', label: 'item 2' },
+  { value: 'vanilla', label: 'item 3' }
+];
+
 function App() {
   const [activeButton, setActiveButton] = useState('');
-  const [openedMenu, setOpenedMenu] = useState('');
+  const [activeField, setActiveField] = useState('');
+  const [openedMenu, setOpenedMenu] = useState(false);
 
   return (
-    <div className="App">
+    <div 
+      className="App"
+      onClick={()=> {
+        setActiveButton('')}}
+    >
       <header className="App-header">
         <nav className="nav">
           <ul className="menu">
@@ -32,70 +48,117 @@ function App() {
               </img>
               </NavLink>
             </li>
-            <li className="menu__item number"
-              onClick={()=> setActiveButton(BUTTONS.one)}
+            <li 
+              className={
+                classNames(
+                  "menu__item", {"number": activeButton !== SELECT.one, "number-active": activeButton === SELECT.one})
+              }
+              onClick={(event)=> {
+                event.stopPropagation();
+                setActiveButton(SELECT.one)}}
             >
-              <button className="button">
-                One
-              </button>
-              {/* <div className={
-                activeButton === BUTTONS.one
-                ? "selector"
-                : "selector__hidden"}
-                >
-                <div className={"selector-button"}>
-                  Item 1
-                </div>
-                <div className={"selector-button"}>
-                  Item 2
-                </div>
-                <div className={"selector-button"}>
-                  Item 3
-                </div>
-              </div> */}
+              <Select 
+                className="select-one"
+                options={options}
+                menuIsOpen={activeButton === SELECT.one}
+                />
             </li>
-            <li className="menu__item number"
-              onClick={()=> setActiveButton(BUTTONS.one)}
+            <li
+              className={
+                classNames("menu__item", "number", {"number-active": activeButton === SELECT.two})
+              }
+              onClick={(event)=> {
+                event.stopPropagation();
+                setActiveButton(SELECT.two);
+              }}
             >
-              <button className="button">
-                Two
-              </button>
+              <Select
+                className="select-two"
+                options={options}
+                menuIsOpen={activeButton === SELECT.two}
+              />
             </li>
-            <li className="menu__item number"
-              onClick={()=> setActiveButton(BUTTONS.one)}
+            <li
+              className={
+                classNames("menu__item", "number", {"number-active": activeButton === SELECT.three})
+              }
+              onClick={(event)=> {
+                event.stopPropagation();
+                setActiveButton(SELECT.three);
+              }}
             >
-              <button className="button">
-                Three
-              </button>
+              <Select
+                className="select-three"
+                options={options}
+                menuIsOpen={activeButton === SELECT.three}
+              />
             </li>
             <li className={classNames({"menu__item": true})}
-              onClick={()=> setActiveButton(BUTTONS.one)}
+              onClick={()=> setActiveButton(SELECT.one)}
             >
               <button className="button button-login">
                 LOG IN
               </button>
             </li>
             <li className={classNames({"menu__item": true})}
-              onClick={()=> setActiveButton(BUTTONS.one)}
+              onClick={()=> setActiveButton(SELECT.one)}
             >
               <button className="button button-sign">
                 SIGN UP
               </button>
             </li>
             <li 
-              className="menu__item burger"
+              className={classNames("burger", {"open": openedMenu})}
               onClick={()=>setOpenedMenu(!openedMenu)}
             >
-              { !openedMenu
-              ? (<img className="burger-image" src={menu}
-                alt="burger-menu"
-              />)
-              : (<img className="close-image" src={closeMenu}
-                  alt="close-menu"
-                />
-              )}
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
             </li>
           </ul>
+          <div className={openedMenu ? "nav__bottom-visible" : "nav__bottom-closed"}>
+            <div 
+              className={classNames("nav__dropdown", {"active": activeField === SELECT.one})}
+              onClick={(event)=> {
+                event.stopPropagation();
+                setActiveField(SELECT.one)
+              }}
+            >
+              <FacebookOutlined className="nav__dropdown-icon"/>
+              <span className="nav__dropdown-text">Facebook</span>
+              <div className={classNames("nav__dropdown-plus", {"active-plus": activeField === SELECT.one})}>
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+            <div 
+              className={classNames("nav__dropdown", {"active": activeField === SELECT.two})}
+              onClick={(event)=> {
+                event.stopPropagation();
+                setActiveField(SELECT.two)}}
+            >
+             <TwitterOutlined className="nav__dropdown-icon"/>
+              <span className="nav__dropdown-text">Twitter</span>
+              <div className={classNames("nav__dropdown-plus", {"active-plus": activeField === SELECT.two})}>
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+            <div 
+              className={classNames("nav__dropdown", {"active": activeField === SELECT.three})}
+              onClick={(event)=> {
+                event.stopPropagation();
+                setActiveField(SELECT.three)}}
+            >
+              <LinkedinOutlined className="nav__dropdown-icon"/>
+              <span className="nav__dropdown-text">LinkedIn</span>
+              <div className={classNames("nav__dropdown-plus", {"active-plus": activeField === SELECT.three})}>
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+          </div>
         </nav>
       </header>
     </div>
@@ -103,15 +166,3 @@ function App() {
 }
 
 export default App;
-
-              {/* <div className={"selector"}>
-                <button className={"selector__block"}>
-                  Item 1
-                </button>
-                <button className={"selector__block"}>
-                  Item 2
-                </button>
-                <button className={"selector__block"}>
-                  Item 3
-                </button>
-              </div> */}
